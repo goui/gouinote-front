4fdf10a6545f3c4a43c6e037607838596ae17b16
goui <guillaume.galliano@gmail.com>
2016-12-09 15:22:48 -0500

    Added recycler view to the note fragment
    This includes the fragment layout, the note item layout and the drawables

--- app/src/main/java/fr/goui/gouinote/main/note/NotesFragment.java
+++ app/src/main/java/fr/goui/gouinote/main/note/NotesFragment.java
@@ -2,24 +2,52 @@ package fr.goui.gouinote.main.note;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import fr.goui.gouinote.R;
 
 /**
- * A simple {@link Fragment} subclass.
+ * The screen showing all the notes.
  */
 public class NotesFragment extends Fragment {
 
     public static final String NOTES = "NOTES";
 
+    @BindView(R.id.progress_bar)
+    ProgressBar mProgressBar;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.swipe_refresh)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(NOTES);
-        return textView;
+        View rootView = inflater.inflate(R.layout.swipe_to_refresh_list, container, false);
+        ButterKnife.bind(this, rootView);
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(null);
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // TODO refresh content
+            }
+        });
+
+        return rootView;
     }
 
 }
